106067a7cac0483e563c018c95fdf4d537a09d81
Sarah Kehoe <sk367@github.com>
2015-02-11 20:23:16 +0000

    added search form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'search/index'
-
-  get 'search/results'
 
   root to: 'visitors#index'
   get 'dashboards/dashboard'
   get 'create_iteneraries/create_itenerary'
   get 'add_events/add_event'
+  get 'search/results'
+  get 'search/index'
+
 end
