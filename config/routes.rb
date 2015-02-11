adfb395fe926ae6ece86d9b403e35d167cf35b04
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-02-11 01:31:24 -0700

    added add_event controller, basic view, some css to help the view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   get 'dashboards/dashboard'
   get 'create_iteneraries/create_itenerary'
+  get 'add_events/add_event'
 end
---
+++ test/helpers/add_events_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AddEventsHelperTest < ActionView::TestCase
+end
