38db9d4bb7600f8fa08e29ffd55dcacb87e519e2
Lane Fujikado <lf237@nau.edu>
2015-02-16 00:42:33 +0000

    main welcome page; some controller functionality

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,13 @@
 class VisitorsController < ApplicationController
-end
+  def welcome
+  end
+  
+  def index
+    x = 3
+    if x == 3
+      render :layout => "welcome"
+    else
+      render :layout => "application"
+    end
+  end
+end  
