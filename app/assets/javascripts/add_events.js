da00c7026eb780b48fd50f1c9b198b5514ad8469
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-02-16 00:50:13 -0700

    added some more scss for the mini calendar

--- app/assets/javascripts/add_events.js
+++ app/assets/javascripts/add_events.js
@@ -4,7 +4,6 @@
 
 function testFunction() {
   
-	alert("moo")
 
 	var minicalendar = jQuery('<div class="mini-calendar"></div>');
 	var table = jQuery('<table></table>');
@@ -12,11 +11,18 @@ function testFunction() {
 	var th1 = jQuery('<th></th>');
 	var leftbutton = jQuery('<button type ="button" onclick="testFunction()">le</button>');
 	jQuery($(th1).append(leftbutton));
-	var th2 = jQuery('<th colspan="5">Month</th>');
-	var th3 = jQuery('<th></th>');
+	//var th2 = jQuery('<th></th>');
+	//var th3 = jQuery('<th></th>');
+	var th4 = jQuery('<th colspan="5">Month</th>');
+	//var th5 = jQuery('<th></th>');
+	//var th6 = jQuery('<th></th>');
+
+	var th7 = jQuery('<th></th>');
 	var rightbutton = jQuery('<button type ="button" onclick="testFunction()">ri</button>');
-	jQuery($(th3).append(rightbutton));
-	jQuery($(tr1).append(th1, th2, th3));
+	jQuery($(th7).append(rightbutton));
+	
+	jQuery($(tr1).append(th1, th4, th7));
+	//jQuery($(tr1).append(th1, th2, th3, th4, th5, th6, th7));
 
 	var tr2 = jQuery('<tr></tr>');
 	var tdday1 = jQuery('<td class="days">S</td>');
@@ -29,20 +35,14 @@ function testFunction() {
 	jQuery($(tr2).append(tdday1, tdday2, tdday3, tdday4, tdday5, tdday6, tdday7));
 
 	var tr3 = jQuery('<tr></tr>');
-	var td1 = jQuery('<td>1</td>');
+	var td1 = jQuery('<td onclick="testFunction()">1</td>');
 	var td2 = jQuery('<td>2</td>');
 	var td3 = jQuery('<td>3</td>');
 	var td4 = jQuery('<td>4</td>');
 	var td5 = jQuery('<td>5</td>');
 	var td6 = jQuery('<td>6</td>');
 	var td7 = jQuery('<td>7</td>');
-	jQuery($(tr3).append(td1));
-	jQuery($(tr3).append(td2));
-	jQuery($(tr3).append(td3));
-	jQuery($(tr3).append(td4));
-	jQuery($(tr3).append(td5));
-	jQuery($(tr3).append(td6));
-	jQuery($(tr3).append(td7));
+	jQuery($(tr3).append(td1, td2, td3, td4, td5, td6, td7));
 
 	var tr4 = jQuery('<tr></tr>');
 	var td8 = jQuery('<td>8</td>');
@@ -52,13 +52,7 @@ function testFunction() {
 	var td12 = jQuery('<td>12</td>');
 	var td13 = jQuery('<td>13</td>');
 	var td14 = jQuery('<td>14</td>');
-	jQuery($(tr4).append(td8));
-	jQuery($(tr4).append(td9));
-	jQuery($(tr4).append(td10));
-	jQuery($(tr4).append(td11));
-	jQuery($(tr4).append(td12));
-	jQuery($(tr4).append(td13));
-	jQuery($(tr4).append(td14));
+	jQuery($(tr4).append(td8, td9, td10, td11, td12, td13, td14));
 
 	var tr5 = jQuery('<tr></tr>');
 	var td15 = jQuery('<td>15</td>');
@@ -68,13 +62,7 @@ function testFunction() {
 	var td19 = jQuery('<td>19</td>');
 	var td20 = jQuery('<td>20</td>');
 	var td21 = jQuery('<td>21</td>');
-	jQuery($(tr5).append(td15));
-	jQuery($(tr5).append(td16));
-	jQuery($(tr5).append(td17));
-	jQuery($(tr5).append(td18));
-	jQuery($(tr5).append(td19));
-	jQuery($(tr5).append(td20));
-	jQuery($(tr5).append(td21));
+	jQuery($(tr5).append(td15, td16, td17, td18, td19, td20, td21));
 
 	var tr6 = jQuery('<tr></tr>');
 	var td22 = jQuery('<td>22</td>');
@@ -84,13 +72,7 @@ function testFunction() {
 	var td26 = jQuery('<td>26</td>');
 	var td27 = jQuery('<td>27</td>');
 	var td28 = jQuery('<td>28</td>');
-	jQuery($(tr6).append(td22));
-	jQuery($(tr6).append(td23));
-	jQuery($(tr6).append(td24));
-	jQuery($(tr6).append(td25));
-	jQuery($(tr6).append(td26));
-	jQuery($(tr6).append(td27));
-	jQuery($(tr6).append(td28));
+	jQuery($(tr6).append(td22, td23, td24, td25, td26, td27, td28));
 
 	var tr7 = jQuery('<tr></tr>');
 	var td29 = jQuery('<td>29</td>');
@@ -100,27 +82,13 @@ function testFunction() {
 	var td33 = jQuery('<td>33</td>');
 	var td34 = jQuery('<td>34</td>');
 	var td35 = jQuery('<td>35</td>');
-	jQuery($(tr7).append(td29));
-	jQuery($(tr7).append(td30));
-	jQuery($(tr7).append(td31));
-	jQuery($(tr7).append(td32));
-	jQuery($(tr7).append(td33));
-	jQuery($(tr7).append(td34));
-	jQuery($(tr7).append(td35));
+	jQuery($(tr7).append(td29, td30, td31, td32, td33, td34, td35));
 
-	jQuery($(table).append(tr1));
-	jQuery($(table).append(tr2));
-	jQuery($(table).append(tr3));
-	jQuery($(table).append(tr4));
-	jQuery($(table).append(tr5));
-	jQuery($(table).append(tr6));
-	jQuery($(table).append(tr7));
+	jQuery($(table).append(tr1, tr2, tr3, tr4, tr5, tr6, tr7));
 
 	var label = jQuery('<label for="startdate1">Start Date</label>');
-	jQuery($(minicalendar).append(label));
-	jQuery($(minicalendar).append(table));
 	var input = jQuery('<input type="text" class="form-control input-lg" id="startdate1">');
-	jQuery($(minicalendar).append(input));
+	jQuery($(minicalendar).append(label, table, input));
 
 	jQuery($(".test").append(minicalendar));
 }
