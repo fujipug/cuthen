a98ae9716e05ba10392e8a81e73fe6ee98034dd1
Lane <lf237@nau.edu>
2015-03-01 16:16:12 -0700

    sign_in + sign_out redirects

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,14 +2,7 @@ class VisitorsController < ApplicationController
   def welcome
     
   end
-  
   def index
-    x = 2
-    if x == 3
-
-      render(:template => 'dashboards/dashboard')
-    else
-      render :layout => "welcome"
-    end
+    render :layout => "welcome"
   end
 end  
