722f1f1c329c23761e48cf457afba6a1443f9f44
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-03 01:27:13 -0700

    made add field btn more abstract for other models

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -298,13 +298,13 @@ function remove_field(btn, input) {
     $("input[name='"+ input + "']").attr({"value" : "", "type" : "hidden"});
     $("#" + btn).remove()
 }
-function add_user_field(btn, moo) {
-  remove_button=$('<button id="remove_field_button_' + moo + '" name="button" onclick="remove_field(\'remove_field_button_"' + moo + '"\', \'event[event_invited_users_attributes]["' + moo + '"][user_id]\')">Remove</button>');
-  label=$('<label class="sr-only" for="event_invited_users_attributes_' + moo + '_user_id">User ID</label>');
-  input=$('<input class="form-control input-lg" id="event_event_invited_users_attributes_' + moo + '_user_id" name="event[event_invited_users_attributes][' + moo + '][user_id]" type="number">');
+function add_user_field(btn, model, nested_id) {
+  remove_button=$('<button id="remove_field_button_' + nested_id + '" name="button" onclick="remove_field(\'remove_field_button_"' + nested_id + '"\', \'event[event_invited_' + model + 's_attributes]["' + nested_id + '"][' + model + '_id]\')">Remove</button>');
+  label=$('<label class="sr-only" for="event_invited_users_attributes_' + nested_id + '_user_id">User ID</label>');
+  input=$('<input class="form-control input-lg" id="event_event_invited_' + model + 's_attributes_' + nested_id + '_' + model + '_id" name="event[event_invited_' + model + 's_attributes][' + nested_id + '][' + model + '_id]" type="number">');
   nested_form_div=$('<div class="form-group"></div>');
   
   nested_form_div.append(remove_button, label, input);
-  $('#event_invited_user_form').append(nested_form_div);
-  $('#' + btn).attr("onclick","add_user_field('add_user_button', '" + (moo + 1) + "')");
+  $('#event_invited_' + model + 's_form').append(nested_form_div);
+  $('#' + btn).attr("onclick","add_" + model + "_field('add_" + model + "_button', '" + model + "', '" + (nested_id + 1) + "')");
 }
