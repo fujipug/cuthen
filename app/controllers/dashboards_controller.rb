34f79d771f54d3bec79606cd0aaa61ed82868cda
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-03 01:04:13 -0700

    uncommented the itinerary part in darhboards controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,6 +3,6 @@ class DashboardsController < ApplicationController
   def dashboard
     @user = current_user
     @event = Event.first  #fix this
-    #@itinerary = Itinerary.find(@event.itinerary_id)
+    @itinerary = Itinerary.find(@event.itinerary_id)
   end
 end
