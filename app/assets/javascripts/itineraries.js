3ca9e301fd09249c09a2006f9bcf1a1fb3400244
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-24 03:11:27 -0700

    abstracted itinerary form

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -8,3 +8,14 @@ var ready = function() {
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+function toggle_label_button(label, toggle) {
+    if (toggle == 0) {
+        //$('label).attr('aria-expanded', 'false');
+        $(label).attr('onclick', 'toggle_label_button(\'' + label + '\', 1)');
+    }
+    else {
+        //$(label).attr('aria-expanded', 'true');
+        $(label).attr('onclick', 'toggle_label_button(\'' + label + '\', 0)');
+    }
+}
