14851b2dd2233ffc836b7eea55c20f64bd826094
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-23 23:08:46 -0700

    calendar populates with event json

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,21 +1,16 @@
 var ready = function() {
-    // page is now ready, initialize the calendar...
-    $('#calendar').fullCalendar({
-    // put your options and callbacks here
-        defaultView: 'agendaWeek',
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-        events: [
-                    {
-                        title: 'Poop',
-                        start: '2015-03-22'
-                }   
-                ]
-
-
-    })
-    //hide calendar after it is done loading its stuff
-    jQuery($('#cal').attr('class', 'hide'));
+    $.get( $("#cal").data("url"), function( data ) {
+        // page is now ready, initialize the calendar...
+        $('#calendar').fullCalendar({
+        // put your options and callbacks here
+            defaultView: 'agendaWeek',
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: data
+        })
+        //hide calendar after it is done loading its stuff
+        jQuery($('#cal').attr('class', 'hide'));
+    });
 };
 $(document).ready(ready);
 $(document).on('page:load', ready);
