4563d70063f7d13c77ff5cc3b892c33c5785d162
Lane <lf237@nau.edu>
2015-03-23 22:23:16 -0600

    calendar agendaweek view

--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -2,6 +2,17 @@ var ready = function() {
     // page is now ready, initialize the calendar...
     $('#calendar').fullCalendar({
     // put your options and callbacks here
+        defaultView: 'agendaWeek',
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+                    {
+                        title: 'Poop',
+                        start: '2015-03-22'
+                }   
+                ]
+
+
     })
     //hide calendar after it is done loading its stuff
     jQuery($('#cal').attr('class', 'hide'));
