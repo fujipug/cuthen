448ef93babcdf4c13024acd77e811d41e1e363cb
Sarah Kehoe <sk367@github.com>
2015-03-27 00:12:48 +0000

    made vote model work with our date thingy

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,33 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  
+    
+  def datetime_to_datestring(datetime)
+  	datetime.try(:strftime, "%m/%d/%Y %I:%M %p")
+  end
+
+  def string_to_datetime(string)
+  	DateTime.strptime(string, "%m/%d/%Y %I:%M %p")
+  end
+
+  def startdate
+  	datetime_to_datestring(start)
+  end
+
+  def startdate=(start_datestring)
+  	self.start_datetime = string_to_datetime(start)
+  end
+
+  def enddate
+    datetime_to_datestring(end_time)
+  end
+
+  def enddate=(end_datestring)
+    self.end_datetime = string_to_datetime(end_time)
+  end
+
+  def as_json(options={})
+    {start: start, end: end_time}
+  end
 end
