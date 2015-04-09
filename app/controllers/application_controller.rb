00bb7238470fbd4d25a6306556d5359f51e9cde4
Lane <lf237@nau.edu>
2015-04-09 13:11:35 -0700

    fixed dashboard path error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   def after_sign_in_path_for(user)
-    dashboards_dashboard_path
+    dashboard_path
   end
   def after_sign_out_path_for(user)
     root_url
