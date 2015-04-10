97d6714346406e7109462ac5954e2f2b232b8133
Sarah Kehoe <sk367@github.com>
2015-04-10 07:24:42 +0000

    changed Vote For Time form

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -22,23 +22,55 @@ class VotesController < ApplicationController
   end
 
   def edit
+    @vote = Vote.new
+    @event = Event.find(params[:event_id])
   end
 
   def create
-    @vote = Vote.new(vote_params)
-    @event = Event.find(params[:event_id])
-    @vote.event_id = @event.id
-    if !@event.votetype
-      @vote.end_time = @vote.start + @event.duration*60
-    end
-    @vote.user_id = current_user.id
-    @event = Event.find(@vote.event_id)
-    #event_invited_users = @event.event_invited_users.build
-    if @vote.save
-      #redirect to events page
-      redirect_to url_for(:controller => :events, :action =>'show', :id => @event.id, :itinerary_id => @event.itinerary_id )
+    #if there's a times array, do the thing for the voting
+    #otherwise, just save the choice for First Come First Serve
+    if params[:times]
+      times = params[:times]
+        times.each do |time|
+          @vote = Vote.new
+          
+          #since this whole process is generally fucked up, just gonna set everything by hand
+          @vote.start = time
+          @event = Event.find(params[:event_id])
+          @vote.end_time = @vote.start + @event.duration*60
+          @vote.event_id = @event.id
+          @vote.user_id = current_user.id
+          
+          #Makes sure user doesn't end up voting twice or something.  What a terrible thing that would be.
+          #Really.
+          #Would just mess everything right up. 
+          if !Vote.where(user_id: @vote.user_id, event_id: @vote.event_id, start: @vote.start).first
+            @vote.save
+          end
+
+        end
+      redirect_to url_for(:controller => :votes, :action =>'index', :itinerary_id => @event.itinerary_id )
+      
+      #the first come first serve bit.
     else
-      render('new')
+      @vote = Vote.new(vote_params)
+      @event = Event.find(params[:event_id])
+      @vote.event_id = @event.id
+      #must manually add an end time if doing the first come first serve
+      if !@event.votetype
+        @vote.end_time = @vote.start + @event.duration*60
+      end
+      @vote.user_id = current_user.id
+      @event = Event.find(@vote.event_id)
+      #event_invited_users = @event.event_invited_users.build
+      if @vote.save
+        #redirect to events page
+        redirect_to url_for(:controller => :events, :action =>'show', :id => @event.id, :itinerary_id => @event.itinerary_id )
+      else
+        redirect_to url_for(:controller => :votes, :action =>'new', :event_id => @event.id, :itinerary_id => @event.itinerary_id )
+        flash[:error] = flash_error_message(@vote)
+        #render('new')
+      end
     end
   end
 
@@ -59,5 +91,9 @@ class VotesController < ApplicationController
 
     def vote_params
       params.require(:vote).permit(:user_id, :event_id, :start, :end_time)
+    end
+  
+    def flash_error_message(arg)
+      "The form contains #{arg.errors.count} error: #{arg.errors.full_messages.join(', ')}"       
     end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,15 +1,35 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  validate :start, presense: true
+  validate :end_time, presense: true
+  validate :end_cannot_come_before_start, :starts_not_before_event, :time_range_less_than_duration
   
-  validate :end_cannot_come_before_start
   
   def end_cannot_come_before_start
     if end_time < start
       errors.add(:end_time, "Invalid Time Range")
     end
   end
+  
+  def starts_not_before_event
+    if start < Event.find(event_id).start_datetime
+      errors.add(:start, "time too early")
+    end
+  end
+  
+  def starts_not_after_event
+    if start > Event.find(event_id).end_datetime
+      errors.add(:start, "time too late")
+    end
+  end
     
+  def time_range_less_than_duration
+    if (end_time - start) < Event.find(event_id).duration*60
+      errors.add(:end_time, "Time span not long enough")
+    end
+  end
+  
   def datetime_to_datestring(datetime)
   	datetime.try(:strftime, "%m/%d/%Y %I:%M %p")
   end
